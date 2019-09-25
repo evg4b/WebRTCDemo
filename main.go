2f17a56bd558b71e11b7ceed6a572d30e2338647
Evgeny Abramovich <evg.abramovitch@gmail.com>
2019-09-25 17:15:09 +0300

    Simple connection

--- main.go
+++ main.go
@@ -1,72 +1,195 @@
 package main
 
 import (
+	"bytes"
 	"log"
-	"net"
 	"net/http"
-	"os"
-	"strconv"
-	"sync"
 	"time"
 
-	"github.com/pion/logging"
-	"github.com/pion/turn"
+	"github.com/gorilla/websocket"
 )
 
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go fileServer(&wg)
-	wg.Add(1)
-	go stunServer(&wg)
-	wg.Wait()
-}
-
-func fileServer(wg *sync.WaitGroup) {
-	defer wg.Done()
 	port := "8010"
 	directory := "./webroot"
+
+	hub := newHub()
+	go hub.run()
+
 	http.Handle("/", http.FileServer(http.Dir(directory)))
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
 	log.Printf("Serving %s on HTTP port: %s\n", directory, port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
-func stunServer(wg *sync.WaitGroup) {
-	defer wg.Done()
+type Hub struct {
+	// Registered clients.
+	clients map[*Client]bool
 
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		os.Stderr.WriteString("Oops: " + err.Error() + "\n")
-		os.Exit(1)
+	// Inbound messages from the clients.
+	broadcast chan []byte
+
+	// Register requests from the clients.
+	register chan *Client
+
+	// Unregister requests from clients.
+	unregister chan *Client
+}
+
+func newHub() *Hub {
+	return &Hub{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
 	}
+}
 
-	for _, a := range addrs {
-		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
-				os.Stdout.WriteString(ipnet.IP.String() + "\n")
+func (h *Hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
 			}
 		}
 	}
+}
 
-	udpPort, err := strconv.Atoi("3478")
-	if err != nil {
-		log.Panic(err)
-	}
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
 
-	channelBindTimeout, _ := time.ParseDuration("30s")
-	s := turn.NewServer(&turn.ServerConfig{
-		// Realm:              realm,
-		ChannelBindTimeout: channelBindTimeout,
-		ListeningPort:      udpPort,
-		LoggerFactory:      logging.NewDefaultLoggerFactory(),
-		Software:           os.Getenv("SOFTWARE"),
-	})
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 60 * time.Second
 
-	err = s.Start()
-	if err != nil {
-		log.Panic(err)
-	}
+	// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
 
+	// Maximum message size allowed from peer.
+	maxMessageSize = 512 * 1000
+)
+
+var (
+	newline = []byte{'\n'}
+	space   = []byte{' '}
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+// Client is a middleman between the websocket connection and the hub.
+type Client struct {
+	hub *Hub
+
+	// The websocket connection.
+	conn *websocket.Conn
+
+	// Buffered channel of outbound messages.
+	send chan []byte
+}
+
+// readPump pumps messages from the websocket connection to the hub.
+//
+// The application runs readPump in a per-connection goroutine. The application
+// ensures that there is at most one reader on a connection by executing all
+// reads from this goroutine.
+func (c *Client) readPump() {
+	defer func() {
+		c.hub.unregister <- c
+		c.conn.Close()
+	}()
+	c.conn.SetReadLimit(maxMessageSize)
+	c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
 	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
+		c.hub.broadcast <- message
 	}
+}
+
+// writePump pumps messages from the hub to the websocket connection.
+//
+// A goroutine running writePump is started for each connection. The
+// application ensures that there is at most one writer to a connection by
+// executing all writes from this goroutine.
+func (c *Client) writePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.conn.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-c.send:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				// The hub closed the channel.
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(message)
+
+			// Add queued chat messages to the current websocket message.
+			n := len(c.send)
+			for i := 0; i < n; i++ {
+				w.Write(newline)
+				w.Write(<-c.send)
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
+
+// serveWs handles websocket requests from the peer.
+func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256)}
+	client.hub.register <- client
+
+	// Allow collection of memory referenced by the caller by doing all work in
+	// new goroutines.
+	go client.writePump()
+	go client.readPump()
 }
--- webroot/index.js
+++ webroot/index.js
@@ -1,49 +1,97 @@
-(async () => {
-    const startBtn = document.getElementById('start');
+const currentId = `f${(~~(Math.random() * 1e8)).toString(16)}`;
+const socked = new WebSocket("ws://172.20.40.91:8010/ws")
+const startBtn = document.getElementById('start');
+const localSdp = document.getElementById('localSdp');
+const remoteSdp = document.getElementById('remoteSdp');
 
-    const connection = new RTCPeerConnection({
-        iceServers: [{ url: 'turn:172.20.40.91:3478' }]
-    });
+const connection = new RTCPeerConnection({
+    iceServers: [
+        { url: 'stun:stun.l.google.com:19302' },
+        { url: 'stun:stun1.l.google.com:19302' },
+        { url: 'stun:stun2.l.google.com:19302' },
+        { url: 'stun:stun3.l.google.com:19302' },
+        { url: 'stun:stun4.l.google.com:19302' },
+        { url: 'stun:stun01.sipphone.com' },
+        { url: 'stun:stun.ekiga.net' },
+        { url: 'stun:stun.fwdnet.net' },
+        { url: 'stun:stun.ideasip.com' },
+        { url: 'stun:stun.iptel.org' },
+        { url: 'stun:stun.rixtelecom.se' },
+        { url: 'stun:stun.schlund.de' },
+        { url: 'stun:stunserver.org' },
+        { url: 'stun:stun.softjoys.com' },
+        { url: 'stun:stun.voiparound.com' },
+        { url: 'stun:stun.voipbuster.com' },
+        { url: 'stun:stun.voipstunt.com' },
+        { url: 'stun:stun.voxgratia.org' },
+        { url: 'stun:stun.xten.com' },
+    ]
+});
 
-    startBtn.addEventListener('click', async () => {
-        console.log('start');
-        startBtn.disabled = true;
-        var offer = await connection.createOffer();
-
-    });
-
-
-
-    // const localSdp = document.getElementById('localSdp');
-    // const localType = document.getElementById('localType');
-    // const setRemote = document.getElementById('setRemote');
-    // const remoteSdp = document.getElementById('remoteSdp');
-    // const remoteType = document.getElementById('remoteType');
-
-    // const connection = new RTCPeerConnection({
-    //     iceServers: [{ url: 'turn:172.20.40.91:3478' }]
-    // });
-    // const offer = await connection.createOffer();
-    // // connection.setLocalDescription(offer);
-    // localType.value = offer.type;
-    // localSdp.value = offer.sdp;
-
-
-    // setRemote.addEventListener('click', async () => {
-    //     var desc = new RTCSessionDescription({
-    //         sdp: remoteSdp.value,
-    //         type: remoteType.value
-    //     });
-    //     connection.setRemoteDescription(desc)
-    //     var a = await connection.createAnswer()
-    //     console.log(a);
-    //     // var a = connection.createDataChannel('dasdas')
-    //     // a.onopen = () => {
-    //     //     console.log("OPEN")
-    //     // }
-    //     // a.send("sadsad");
-    // })
+socked.onopen = () => {
+    console.log("OPEN")
+}
 
 
-})();
 
+
+let ddd = false;
+socked.onerror = (event) => {
+    console.log("ERROR", event)
+}
+
+socked.onmessage = (event) => {
+    console.log(event);
+    const { id, type, data } = JSON.parse(event.data);
+    if (id != currentId) {
+        handle(type, data)
+    }
+}
+
+
+async function handle(type, data) {
+    switch (type) {
+        case 'offer':
+            remoteSdp.value = data.sdp;
+            connection.setRemoteDescription(data);
+            const answer = await connection.createAnswer()
+            connection.setLocalDescription(answer);
+            localSdp.value = answer.sdp;
+            sendMessage('answer', answer)
+            ddd = true;
+            break;
+        case 'answer':
+            connection.setRemoteDescription(data);
+            remoteSdp.value = data.sdp;
+            ddd = true;
+            break;
+        case 'ice':
+            connection.addIceCandidate(data);
+            break;
+    }
+}
+
+var ch = connection.createDataChannel('demo');
+
+startBtn.addEventListener('click', async () => {
+    var offer = await connection.createOffer();
+    connection.setLocalDescription(offer);
+    localSdp.value = offer.sdp;
+    sendMessage('offer', offer);
+});
+
+connection.onicecandidate = async (event) => {
+    if (event.candidate && ddd) {
+        // sendMessage('ice', event.candidate)
+    }
+}
+
+ch.onopen = () => alert("УРААА")
+
+ch.onmessage = console.log
+console.log(connection)
+console.log(ch)
+
+function sendMessage(type, data) {
+    socked.send(JSON.stringify({ type, data, id: currentId }))
+}
